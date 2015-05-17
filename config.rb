1555a1c7694fe7b99f65095e6917a36f823e86c4
Dan Simmons <dan@simmons.io>
2015-05-17 12:10:36 -0400

    Changed file naming convention and corresponding directory structure to include only the title and to instead be located under source/posts.

--- config.rb
+++ config.rb
@@ -8,9 +8,9 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "{title}.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = "posts/{title}.html"
   # blog.taglink = "tags/{tag}.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
