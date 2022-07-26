2e875b78f92b8e0f1fde75288d6c3bcd97c0f856
Dan Simmons <dan@simmons.io>
2022-07-25 21:54:30 -0600

    Made use of new Header and Footer components.

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,5 +1,11 @@
+import Header from '../../components/header'
+import Footer from '../../components/footer'
+
 export default function PostList() {
   return (
-    <></>
+    <>
+      <Header />
+      <Footer />
+    </>
   )
 }
