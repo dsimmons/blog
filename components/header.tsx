71649434a12163384826aee8ebcbbbbd42b6f00f
Dan Simmons <dan@simmons.io>
2022-07-25 22:07:25 -0600

    Made blog title navigate back to /

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,11 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <header>
-      <h1>/dev/simmonsd</h1>
+      <h1>
+        <Link href="/">
+          /dev/simmonsd
+        </Link>
+      </h1>
       <nav>
         <div>
           <Link href="/posts">
