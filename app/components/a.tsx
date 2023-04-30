681e4f87e16ded87cd5411272253a988f946557d
Dan Simmons <dan@simmons.io>
2023-04-29 22:26:46 -0600

    Added a styled anchor component.

---
+++ app/components/a.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export function A({ children, className = "", href, ...props }) {
+  return (
+    <Link
+      href={href}
+      className={`border-b border-gray-300 text-gray-600 transition-[border-color] hover:border-gray-600 dark:border-gray-500 dark:text-white dark:hover:border-white ${className}`}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
