892a42862ba40b807dc5b50da418aaaf9178b0b2
Dan Simmons <dan@simmons.io>
2023-04-30 18:16:58 -0600

    Added a styled H1 component.

---
+++ app/components/h1.tsx
@@ -0,0 +1,5 @@
+export function H1({ children }) {
+  return (
+    <h1 className="mb-1 text-2xl font-bold dark:text-gray-100">{children}</h1>
+  );
+}
--- mdx-components.ts
+++ mdx-components.ts
@@ -1,10 +1,12 @@
 import type { MDXComponents } from "mdx/types";
 
 import { A as a } from "app/components/a";
+import { H1 as h1 } from "app/components/h1";
 
 export function useMDXComponents(components: MDXComponents) {
   return {
     ...components,
     a,
+    h1,
   } as MDXComponents;
 }
